7371ef4e9ac0851bd0b3bbbded65fcb0baa06a23
Kmac01 <117133875+Kmac01@users.noreply.github.com>
2023-08-12 21:05:21 -0400

    Initial commit

---
+++ src/main/java/pet/store/entity/Customer.java
@@ -0,0 +1,36 @@
+package pet.store.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Customer {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long customer_id;
+	
+	private String customer_first_name;
+	private String customer_last_name;
+	private String customer_email;
+	
+	
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	@ManyToMany(mappedBy = "customers", cascade = CascadeType.PERSIST)
+	private Set<PetStore> petStores = new HashSet<>();
+}
+
+
